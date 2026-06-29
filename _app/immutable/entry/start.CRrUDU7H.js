578581d5f950874dedbd6e8b836863288436fd47
cybabun1 <eva.evergreene@proton.me>
2026-06-29 02:09:04 -0600

    Fix: prepend base path to all nav links

--- _app/immutable/entry/start.CRrUDU7H.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/CwZAd8kM.js";import{y as t}from"../chunks/DG8AwM8X.js";export{t as load_css,r as start};
---
+++ _app/immutable/entry/start.DnXApFXh.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CDPjdjvy.js";import{y as t}from"../chunks/B2yc3TTS.js";export{t as load_css,r as start};
