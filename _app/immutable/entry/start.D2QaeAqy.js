533234e82df1cec91f752ef61bdcc3318fd3b43c
cybabun1 <eva.evergreene@proton.me>
2026-06-29 02:23:22 -0600

    Fix: title/meta fallback, OG tags, page titles take precedence

---
+++ _app/immutable/entry/start.D2QaeAqy.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/xmMfd5mq.js";import{y as t}from"../chunks/Dys9omb9.js";export{t as load_css,r as start};
--- _app/immutable/entry/start.DnXApFXh.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/CDPjdjvy.js";import{y as t}from"../chunks/B2yc3TTS.js";export{t as load_css,r as start};
